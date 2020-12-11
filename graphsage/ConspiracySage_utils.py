571c07dc9659356e361b33694a916d062944088a
Noah <22361216+PurpleAvatar@users.noreply.github.com>
2020-12-11 16:44:01 -0500

    binning conspiracy scores into classes

--- graphsage/ConspiracySage_utils.py
+++ graphsage/ConspiracySage_utils.py
@@ -1,8 +1,9 @@
 import numpy as np
+import scipy.sparse
 import torch
 
 FEAT_PATH = "../data/bag_features.npz"
-ADJ_PATH = "../data/adj_matrix.npz"
+ADJ_PATH = "../data/boosted_adj_matrix.npy"
 SCORE_PATH = "../data/valid_scores.npy"
 
 def convert_sparse_matrix_to_sparse_tensor(X):
@@ -13,10 +14,10 @@ def convert_sparse_matrix_to_sparse_tensor(X):
     return i
 
 def load_features() : 
-    return 
+    return scipy.sparse.load_npz(FEAT_PATH)
 
 def load_adj() : 
-    return 
+    return np.load(ADJ_PATH)
 
 def score_classes(score) : 
     if float(score) < 0.5 : 
@@ -28,16 +29,18 @@ def score_classes(score) :
     else : 
         return 3
 
-def load_scores() : 
+def load_classes() : 
     # bin the scores 0|0.5|0.65|0.85|1.0 into classes
-    
+    scores = np.load(SCORE_PATH)
+    classes = [score_classes(s) for s in scores]
+
     # print stats
     print("Created these class divisions: ")
-    print("Class 0 ", len(np.where(scores == 0)[0]))
-    print("Class 1 ", len(np.where(scores == 1)[0]))
-    print("Class 2 ", len(np.where(scores == 2)[0]))
-    print("Class 3 ", len(np.where(scores == 3)[0]))
-    return 
+    print("Class 0 ", len(np.where(classes == 0)[0]))
+    print("Class 1 ", len(np.where(classes == 1)[0]))
+    print("Class 2 ", len(np.where(classes == 2)[0]))
+    print("Class 3 ", len(np.where(classes == 3)[0]))
+    return classes
 
 # Return the pytorch geometric data object
 def load_YouTube() : 
@@ -46,13 +49,13 @@ def load_YouTube() :
     # grab the adjacency matrix
     adj = load_adj() 
     # grab the conspiracy scores
-    scores = load_scores() 
+    classes = load_classes() 
 
     x = torch.FloatTensor(features)
     edge_index = convert_sparse_matrix_to_sparse_tensor(adj)
 
     # Convert to data object, just for cleanliness
     data = torchData.Data(x=x, edge_index=edge_index)
-    data.y = torch.tensor(scores, dtype=torch.long)
+    data.y = torch.tensor(classes, dtype=torch.long)
 
     return data
